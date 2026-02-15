641565dce7c9acda8b90c6b8694c4e10fc660738
rsunu <rabinnsunuwar@Gmail.com>
2026-02-15 17:33:23 +0000

    Chapter 7: Updated index view and template to display categories from the database

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+# Customize page admin to show more columns
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# Customize Category admin
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Register the models with their respective customised Admin classes
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    # Query the database for a list of all categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if at least 5 don't exist.
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
+    # Return a rendered response to send to the client.
+    return render(request, 'rango/index.html', context=context_dict)
