e9fdb12b147e92928f197059969258e0379a6ca6
rsunu <rabinnsunuwar@Gmail.com>
2026-02-15 17:54:06 +0000

    Chapters 4 & 7: Completed About template and linked to Index

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-from rango.models import Category
+from django.http import HttpResponse 
+from rango.models import Category, Page # Added Page here to fix potential future errors
 
 def index(request):
-    # Query the database for a list of all categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if at least 5 don't exist.
     category_list = Category.objects.order_by('-likes')[:5]
-    
-    # Construct a dictionary to pass to the template engine as its context.
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-    
-    # Return a rendered response to send to the client.
     return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+        
+    return render(request, 'rango/category.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
